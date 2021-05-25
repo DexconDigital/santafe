1b6a46bbcd0630e36ef37b54461ae5b009b99a84
brayanrojas21 <80425297+brayanrojas21@users.noreply.github.com>
2021-05-25 17:26:10 -0500

    Cambio a pdo

--- Santafe_Admin/validar.php
+++ Santafe_Admin/validar.php
@@ -1,32 +1,34 @@
 <?php
 
-require("admin/conexion.php");
+require( "admin/conexion.php" );
 
+$con = conect();
 
-$con=conect();
-
-
-$nombre=$_REQUEST["user"];
-$password=$_REQUEST["pass"];
+$nombre = $_REQUEST["user"];
+$password = $_REQUEST["pass"];
 $id_inmobiliaria = 14;
 
-$qry="SELECT * FROM usuarios WHERE usuario ='$nombre' and password ='$password' and id_inmobiliaria1 = '$id_inmobiliaria'";
-$sql=mysqli_query($con,$qry);
+$qry = "SELECT * FROM usuarios WHERE usuario ='$nombre' and password ='$password' and id_inmobiliaria1 = '$id_inmobiliaria'";
+$result = $con->prepare( $qry );
+$result->execute();
 
-$row = mysqli_fetch_row($sql);
+$row = $result->fetch( PDO::FETCH_OBJ );
+$rowcount = $result->rowCount();
 
-if (mysqli_num_rows($sql) > 0) {
+if ( $rowcount > 0 ) {
     // output data of each row
-    session_start(); 
-      //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario 
-      $_SESSION["autentica"] = "SIP"; 
-      $_SESSION["usuarioactual"] =  $row[0]; 
-     header ("Location: admin/index.php");
-      //nombre del usuario logueado. 
-      //Direccionamos a nuestra página principal del sistema. 
+    session_start();
+
+    //Guardamos dos variables de sesión que nos auxiliará para saber si se está o no "logueado" un usuario
+    $_SESSION["autentica"] = "SIP";
+
+    $_SESSION["usuarioactual"] =  $row;
+
+    header ( "Location: admin/index.php" );
+    //nombre del usuario logueado.
+    //Direccionamos a nuestra página principal del sistema.
 
 } else {
-        
- header ("Location: index.php");
+    header ( "Location: index.php" );
 }
 ?>
